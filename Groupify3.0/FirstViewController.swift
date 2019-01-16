c51d458056a38a05c3cd8ee3453f3591003cee7c
Masik <maz@helenas-macbook-pro.local>
2019-01-16 10:26:17 -0600

    Initial Commit

---
+++ Groupify3.0/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Groupify3.0
+//
+//  Created by Masik on 1/16/19.
+//  Copyright © 2019 District196.org. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Groupify3.0/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Groupify3.0
+//
+//  Created by Masik on 1/16/19.
+//  Copyright © 2019 District196.org. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
