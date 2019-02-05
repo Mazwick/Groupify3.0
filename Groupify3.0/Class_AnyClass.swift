2f38f0be9aa05aaf0219497c4037d2415d3b8a92
Masik <maz@helenas-macbook-pro.local>
2019-02-05 10:33:48 -0600

    Classes bitch

---
+++ Groupify3.0/Class_AnyClass.swift
@@ -0,0 +1,23 @@
+//
+//  Class_AnyClass.swift
+//  Groupify
+//
+//  Created by Masik on 2/4/19.
+//  Copyright © 2019 District196.org. All rights reserved.
+//
+
+import Foundation
+
+class AnyClass
+{
+    //var className: String
+    //var arrrraaayyyyy
+    var classSize: Int
+    
+    init(sizeClass: Int)
+    {
+        classSize = sizeClass
+        
+        
+    }
+}
