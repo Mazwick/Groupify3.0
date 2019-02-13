736aa67c2e3d8420a7d06ceb73413ad2493c6885
Masik <maz@helenas-macbook-pro.local>
2019-02-13 10:32:57 -0600

    Edit student view

---
+++ Groupify3.0/EditStudentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditStudentViewController.swift
+//  Groupify
+//
+//  Created by Masik on 2/13/19.
+//  Copyright © 2019 District196.org. All rights reserved.
+//
+
+import UIKit
+
+class EditStudentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
